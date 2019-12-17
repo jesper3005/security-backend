5254664582c4e2da7947eea06c9244a95f4ab2de
Michael Due Pedersen <mdp@Danskbilferie.local>
2019-12-17 11:00:19 +0100

    Mail service

---
+++ src/main/java/utils/Key.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Key {
+
+    private static String email = "";
+    private static String password = "";
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+}
